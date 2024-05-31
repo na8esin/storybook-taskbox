bc1ce5fa820201113e4d1a4a3ec600019646cd9e
sikmi_watanabe <na8esin.mpst@gmail.com>
2024-05-31 14:05:09 +0900

    npx storybook automigrate

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,15 +2,24 @@
 const config = {
   stories: ['../src/components/**/*.stories.@(js|jsx)'],
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     '@storybook/addon-a11y',
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
