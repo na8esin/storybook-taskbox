30bc0412268ab1b61ca5a1d726d7e5d379c59de8
sikmi_watanabe <na8esin.mpst@gmail.com>
2024-06-02 16:46:47 +0900

    store.jsの修正漏れ

--- src/lib/store.js
+++ src/lib/store.js
@@ -11,14 +11,8 @@ import {
  * The initial state of our store when the app loads.
  * Usually, you would fetch this from a server. Let's not worry about that now
  */
-const defaultTasks = [
-  { id: "1", title: "Something", state: "TASK_INBOX" },
-  { id: "2", title: "Something more", state: "TASK_INBOX" },
-  { id: "3", title: "Something else", state: "TASK_INBOX" },
-  { id: "4", title: "Something again", state: "TASK_INBOX" },
-];
 const TaskBoxData = {
-  tasks: defaultTasks,
+  tasks: [],
   status: "idle",
   error: null,
 };
@@ -58,7 +52,6 @@ const TasksSlice = createSlice({
       }
     },
   },
-  
   /*
    * Extends the reducer for the async actions
    * You can read more about it at https://redux-toolkit.js.org/api/createAsyncThunk
